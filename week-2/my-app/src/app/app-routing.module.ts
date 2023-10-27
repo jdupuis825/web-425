bc7c3381f3b278c3b8055ab2c34e8145e54691cb
Jocelyn <jdupuis@my365.bellevue.edu>
2023-10-26 20:56:07 -0400

    commit

--- week-2/my-app/src/app/app-routing.module.ts
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule, Component } from '@angular/core';
+// Name: Jocelyn Dupuis
+// File: app-routing.module.ts
+// Date: 10/26/2023
+// Description: routing for my app - exercise 2.2
+
+
+// imports
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 
-
+// new paths added to routes array
 const routes: Routes = [
   {
     path: '',
