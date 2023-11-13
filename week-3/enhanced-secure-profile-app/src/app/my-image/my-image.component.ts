40da1a257eccb1a626a12ca1243f315663be6405
Jocelyn <jdupuis@my365.bellevue.edu>
2023-11-12 19:40:57 -0500

    update with code attribution

--- week-3/enhanced-secure-profile-app/src/app/my-image/my-image.component.ts
+++ week-3/enhanced-secure-profile-app/src/app/my-image/my-image.component.ts
@@ -1,10 +1,19 @@
+// Name: Jocelyn Dupuis
+// File: my-details.component.ts
+// Date: 10/26/2023
+// Description: TS file for my images component
+
+// import statements
 import { Component, OnInit } from '@angular/core';
 
+//component 
 @Component({
   selector: 'app-my-image',
   templateUrl: './my-image.component.html',
   styleUrls: ['./my-image.component.css']
 })
+
+//exports MyImageComponent
 export class MyImageComponent implements OnInit {
 
   constructor() { }
